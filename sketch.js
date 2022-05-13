fbb70ee7a6d460be8e7458537d6b0e9d3ac07175
Rodrigo Nunes de Santana <97761598+rnsantana@users.noreply.github.com>
2022-05-12 22:10:20 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 400);
+  somDaTriha.loop();
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentaAtor();
+  voltaPosicaoInicialDoCarro();
+  verificaColisao();
+  incluiPontos();
+  marcaPonto();
+}
